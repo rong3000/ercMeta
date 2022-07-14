30b8276647f36abc4cf9265465163b5ff8a7642c
rong3000 <rong.lin3000@gmail.com>
2022-07-14 13:16:16 +1000

    merged dealt

--- app.py
+++ app.py
@@ -36,128 +36,76 @@ NUMBER_ATTRIBUTES = [1, 2, 1, 1]
 @app.route('/api/element/<token_id>')
 def element(token_id):
     token_id = int(token_id)
-    element_name = "element ""%s" % token_id
-
-    base = BASES[token_id % len(BASES)]
-    eyes = EYES[token_id % len(EYES)]
-    mouth = MOUTH[token_id % len(MOUTH)]
-    # image_url = _compose_image(['images/bases/base-%s.png' % base,
-    #                             'images/eyes/eyes-%s.png' % eyes,
-    #                             'images/mouths/mouth-%s.png' % mouth],
-    #                            token_id, "element")
-    image_url = _get_element_image(token_id)
-
-    attributes = []
-    # _add_attribute(attributes, 'base', BASES, token_id)
-    # _add_attribute(attributes, 'eyes', EYES, token_id)
-    # _add_attribute(attributes, 'mouth', MOUTH, token_id)
-    # _add_attribute(attributes, 'level', INT_ATTRIBUTES, token_id)
-    # _add_attribute(attributes, 'stamina', FLOAT_ATTRIBUTES, token_id)
-    # _add_attribute(attributes, 'personality', STR_ATTRIBUTES, token_id)
-    # _add_attribute(attributes, 'aqua_power', BOOST_ATTRIBUTES, token_id, display_type="boost_number")
-    # _add_attribute(attributes, 'stamina_increase', PERCENT_BOOST_ATTRIBUTES, token_id, display_type="boost_percentage")
-    # _add_attribute(attributes, 'generation', NUMBER_ATTRIBUTES, token_id, display_type="number")
-
-    ELEMENTS = [
-        {
-            'trait_type': 'base',
-            'value': ['jellyfish', 'starfish', 'crab', 'narwhal', 'tealfish', 'goldfish']
-        },
-        {
-            'trait_type': 'eyes',
-            'value': ['big', 'joy', 'wink', 'sleepy', 'content', 'watery']
-        },
-        {
-            'trait_type': 'mouth',
-            'value': ['happy', 'surprised', 'pleased', 'cute', 'sad', 'furious']
-        },
-        {
-            'trait_type': 'arms',
-            'value': ['long', 'short', 'bulky', 'slim', 'X long', 'x short']
-        },
-        {
-            'trait_type': 'legs',
-            'value': ['long', 'short', 'bulky', 'slim', 'X long', 'x short']
-        },
-        {
-            'trait_type': 'background',
-            'value': ['red', 'yellow', 'green', 'blue', 'white', 'purple']
-        },
-    ]
-
-    _get_element_attribute(attributes, token_id)
-
-    # _add_attribute(attributes, ELEMENTS[token_id % len(
-    #     ELEMENTS)]['trait_type'], ELEMENTS[token_id % len(ELEMENTS)]['value'], token_id)
-
-    return jsonify({
-        'name': element_name,
-        'description': "One of the Poo's basic elements",
-        'image': image_url,
-        'attributes': attributes
-    })
-
-
-@app.route('/api/merged/<token_id>')
-def merged(token_id):
-    tokenId = token_id
-    token_ids = []
-
-    while tokenId:
-        if int(tokenId[(len(tokenId)-4):]) > 0:
-            token_ids.append(int(tokenId[(len(tokenId)-4):]))
-        tokenId = tokenId[:(len(tokenId)-4)]
-    token_ids = list(set(token_ids))
-    token_ids = sorted(token_ids, key=lambda a: (a-1) % 10)
-    print(token_ids)
-    uniqueId = ''
-    for id in reversed(token_ids):
-        paddedId = str(id).zfill(4)
-        uniqueId += paddedId
-
-    merged_name = "merged ""%s" % uniqueId
-    bucket = _get_bucket()
-    filename = f'merged/{uniqueId}.json'
-    stats = storage.Blob(bucket=bucket, name=filename).exists()
-    if stats:
-        blobIn = bucket.blob(f"merged/{uniqueId}.json")
-        data = json.loads(blobIn.download_as_string(client=None))
-        return data
-    else:
-        image_url = _compose_image(token_ids, uniqueId, "merged")
-
+    if token_id < 10000:
+        element_name = "element ""%s" % token_id
+        image_url = _get_element_image(token_id)
         attributes = []
-
-        _add_attribute(attributes, token_ids)
-
-        dataServed = json.dumps({
-            'name': merged_name,
-            'description': "Merged Poo",
+        _get_element_attribute(attributes, token_id)
+        return jsonify({
+            'name': element_name,
+            'description': "One of the Poo's basic elements",
             'image': image_url,
             'attributes': attributes
         })
-        print("dataServed is ""%s" % dataServed)
-
-        blobOut = bucket.blob(f"merged/{uniqueId}.json")
-        blobOut.upload_from_string(dataServed)
-        # with tempfile.NamedTemporaryFile(suffix='.json') as tempOut:
-        #     dataServed.save(tempOut.name)
-        #     blobOut = bucket.blob(f"merged/{token_id}.json")
-        #     blobOut.upload_from_filename(filename=tempOut.name)
-        if int(token_id) > 10000:
-            return jsonify({
+    else:
+        tokenId = str(token_id)
+        token_ids = []
+    
+        while tokenId:
+            if int(tokenId[(len(tokenId)-4):]) > 0:
+                token_ids.append(int(tokenId[(len(tokenId)-4):]))
+            tokenId = tokenId[:(len(tokenId)-4)]
+        token_ids = list(set(token_ids))
+        token_ids = sorted(token_ids, key=lambda a: (a-1) % 10)
+        print(token_ids)
+        uniqueId = ''
+        for id in reversed(token_ids):
+            paddedId = str(id).zfill(4)
+            uniqueId += paddedId
+    
+        merged_name = "merged ""%s" % uniqueId
+        bucket = _get_bucket()
+        filename = f'merged/{uniqueId}.json'
+        stats = storage.Blob(bucket=bucket, name=filename).exists()
+        if stats:
+            blobIn = bucket.blob(f"merged/{uniqueId}.json")
+            data = json.loads(blobIn.download_as_string(client=None))
+            return data
+        else:
+            image_url = _compose_image(token_ids, uniqueId, "merged")
+    
+            attributes = []
+    
+            _add_attribute(attributes, token_ids)
+    
+            dataServed = json.dumps({
                 'name': merged_name,
                 'description': "Merged Poo",
                 'image': image_url,
                 'attributes': attributes
             })
-        else:
-            return jsonify({
-                'name': "not exist",
-                'description': "Merged Poo",
-                'image': "not exist",
-                'attributes': "not exist"
-            })
+            print("dataServed is ""%s" % dataServed)
+    
+            blobOut = bucket.blob(f"merged/{uniqueId}.json")
+            blobOut.upload_from_string(dataServed)
+            if int(token_id) > 10000:
+                return jsonify({
+                    'name': merged_name,
+                    'description': "Merged Poo",
+                    'image': image_url,
+                    'attributes': attributes
+                })
+            else:
+                return jsonify({
+                    'name': "not exist",
+                    'description': "Merged Poo",
+                    'image': "not exist",
+                    'attributes': "not exist"
+                })
+
+# @app.route('/api/merged/<token_id>')
+# def merged(token_id):
+
 
 def _get_element_attribute(existing, token_id):
     bucket = _get_bucket()
