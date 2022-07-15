2739bc8ca8f6fb8ed2da7f00d255d410aa86ddfe
rong3000 <rong.lin3000@gmail.com>
2022-07-15 17:16:38 +1000

    allow https://pl-9yi.pages.dev

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ GOOGLE_STORAGE_PROJECT = os.environ['GOOGLE_STORAGE_PROJECT']
 GOOGLE_STORAGE_BUCKET = os.environ['GOOGLE_STORAGE_BUCKET']
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:5000"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "https://pl-9yi.pages.dev"}})
 
 BASES = ['jellyfish', 'starfish', 'crab', 'narwhal', 'tealfish', 'goldfish']
 EYES = ['big', 'joy', 'wink', 'sleepy', 'content']
