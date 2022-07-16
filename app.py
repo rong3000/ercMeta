2919856876107f0c7cf3f38385314e5bc084d1f5
rong3000 <rong.lin3000@gmail.com>
2022-07-16 19:21:55 +1000

    cors is localhost 5000

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ GOOGLE_STORAGE_PROJECT = os.environ['GOOGLE_STORAGE_PROJECT']
 GOOGLE_STORAGE_BUCKET = os.environ['GOOGLE_STORAGE_BUCKET']
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "https://poos.io"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:5000"}})
 
 BASES = ['jellyfish', 'starfish', 'crab', 'narwhal', 'tealfish', 'goldfish']
 EYES = ['big', 'joy', 'wink', 'sleepy', 'content']
